266960f4c8debfd56d073e14658d8e8924fbd3b8
Esra Çetintaş <Esra.Cetintas@outlook.com.tr>
2022-11-03 23:19:48 +0300

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 4;
+            double d = 4.0;
+            string s = "HackerRank ";
+
+            int j = Convert.ToInt32(Console.ReadLine());
+            double f = Convert.ToDouble(Console.ReadLine());
+            string m = Console.ReadLine();
+
+            Console.WriteLine(i + j);
+            Console.WriteLine(String.Format("{0:0.0}", d + f));
+            Console.WriteLine(s + "" + m);
+
+        }
+    }
+}
