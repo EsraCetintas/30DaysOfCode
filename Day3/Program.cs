91ebbb125db8b869902b16a414c6aee328c1dea2
Esra Çetintaş <Esra.Cetintas@outlook.com.tr>
2022-11-04 23:28:12 +0300

    Day3 was completed

---
+++ Day3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine().Trim());
+            if (N % 2 != 0)
+                Console.WriteLine("Weird");
+            else if (N > 2 && N < 5)
+                Console.WriteLine("Not Weird");
+            else if (N > 6 && N < 20)
+               Console.WriteLine("Weird");
+            else
+                Console.WriteLine("Not Weird");
+        }
+    }
+}
